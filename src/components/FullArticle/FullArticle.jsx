c5668293c77f1f9c1618664501d69df7ccfef79c
AlistairHopson <hopson.alistair@gmail.com>
2022-07-28 14:24:39 +0100

    add comments to article

--- src/components/FullArticle/FullArticle.jsx
+++ src/components/FullArticle/FullArticle.jsx
@@ -3,20 +3,42 @@ import "./FullArticle.css";
 import { useParams } from "react-router-dom";
 
 import getArticleById from "../api-interactions/getArticleById";
+import getArticleComments from "../api-interactions/getArticleComments";
 import VoteButtons from "./voteButtons/VoteButtons";
-
+import CommentCard from "./voteButtons/ArticleComments/CommentCard/CommentCard";
 import { useEffect, useState } from "react";
+import "./CentralLoader.css";
 
 export default function FullArticle() {
   let { article_id } = useParams();
 
+  const [isLoading, setIsLoading] = useState(true);
+
   const [article, setArticle] = useState([]);
+  const [comments, setComments] = useState([]);
+  const [toggledComments, setToggledComments] = useState(true);
 
   useEffect(() => {
     getArticleById(article_id).then(({ article }) => {
       setArticle(article);
     });
-  }, []);
+  }, [article_id]);
+
+  useEffect(() => {
+    setIsLoading(true);
+    getArticleComments(article_id).then(({ comments }) => {
+      setComments(comments);
+      setIsLoading(false);
+    });
+  }, [article_id]);
+
+  if (isLoading) {
+    return (
+      <>
+        <div className="central-loader"></div>
+      </>
+    );
+  }
 
   return (
     <div className="article">
@@ -25,8 +47,37 @@ export default function FullArticle() {
       <p>{article.body}</p>
       <div className="votes-and-comments">
         <VoteButtons article={article} />
-        <p>{article.comment_count} comments</p>
+        <div className="comments">
+          <p className="comment-count">{article.comment_count} </p>
+          <button
+            className={
+              toggledComments ? "toggled-comments" : "untoggled-comments"
+            }
+            onClick={() => {
+              setToggledComments(toggledComments ? false : true);
+            }}
+          >
+            <span className="material-icons">chat</span>
+          </button>
+        </div>
       </div>
+
+      {comments.map(({ author, body, created_at, votes, comment_id }) => {
+        if (toggledComments)
+          return (
+            <>
+              <hr />
+              <CommentCard
+                key={comment_id}
+                author={author}
+                body={body}
+                created_at={created_at}
+                votes={votes}
+              />
+              <hr />
+            </>
+          );
+      })}
     </div>
   );
 }
